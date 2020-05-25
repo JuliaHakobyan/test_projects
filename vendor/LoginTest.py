75691988a17b8cc98ee9c4a960fc0834dcd8d770
juliahakobyan <hakobyan.julieta@yahoo.com>
2020-05-25 19:32:52 +0400

    test 5

--- vendor/LoginTest.py
+++ vendor/LoginTest.py
@@ -8,6 +8,10 @@ with Chrome() as driver:
     title = driver.title
     print(title)
     print(url)
+    print(url)
+    print(url)
+    print(url)
+    print(url)
 
     assert 'The Internet' in driver.title
     login_field = driver.find_element_by_id("username")
